af77c9ef7de7cefc3e7de6af02c6692f3395764e
max@dropcreations.com <you@example.com>
2018-11-02 11:38:18 +0000

    Fix error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'max-photo-app.herokuapp.com', :protocol => 'https'}
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true1
+  config.action_controller.perform_caching = true
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
