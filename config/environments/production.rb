5560ec3fd49d3fd4e9033ee88527c7094f1542f3
max@dropcreations.com <you@example.com>
2018-11-02 11:15:54 +0000

    change mailer devivery action URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,10 +10,10 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
    config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'https://max-photo-app.herokuapp.com', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'max-photo-app.herokuapp.com', :protocol => 'https'}
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = true1
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
